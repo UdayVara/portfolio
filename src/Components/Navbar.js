f0e5908d226d5b5b3e8fac105f8509bc75f568a3
Uday vara <varaudayd@gmail.com>
2023-07-11 19:10:53 +0530

    Fixed Closing of Navbar in Mobile VIew

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -3,11 +3,14 @@ import React from 'react';
 
 
 export default function Navbar(props) {
+  const closeNavbar = () => {
+    document.getElementById("navCloseBtn").click()
+  }
   return (
     <nav className="navbar navbar-expand-lg fixed-top" style={{backgroundColor:"#083749"}}>
   <div className="container-fluid ">
     <a className="navbar-brand ms-1 ms-md-4 fs-3" href="#" style={{color:"#f0f0e6"}}>{props.name}</a>
-    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <button className="navbar-toggler" id='navCloseBtn' type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
     <i className="bi bi-list text-light"></i>
     </button>
     <div className="collapse navbar-collapse" id="navbarNav">
@@ -17,16 +20,16 @@ export default function Navbar(props) {
         </li>
         
         <li className="nav-item mx-md-3">
-          <a className="nav-link" href="#about" style={{color:"#f0f0e6"}} >About</a>
+          <a className="nav-link" href="#about" style={{color:"#f0f0e6"}} onClick={closeNavbar} >About</a>
         </li>
         <li className="nav-item mx-md-3">
-          <a className="nav-link" href="#skills" style={{color:"#f0f0e6"}}>Skills</a>
+          <a className="nav-link" href="#skills" style={{color:"#f0f0e6"}} onClick={closeNavbar}>Skills</a>
         </li>
         <li className="nav-item mx-md-3">
-          <a className="nav-link" href="#portfolio" style={{color:"#f0f0e6"}}>Portfolio</a>
+          <a className="nav-link" href="#portfolio" style={{color:"#f0f0e6"}} onClick={closeNavbar}>Portfolio</a>
         </li>
         <li className="nav-item mx-md-3">
-          <a className="nav-link" href="#Contact" style={{color:"#f0f0e6"}}>Contact </a>
+          <a className="nav-link" href="#Contact" style={{color:"#f0f0e6"}} onClick={closeNavbar}>Contact </a>
         </li>
       </ul>
     </div>
