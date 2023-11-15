d567870325a9c927c3afb630373daa919f8ed796
Uday vara <varaudayd@gmail.com>
2023-11-15 19:38:12 +0530

    added tilt effect

--- src/Components/About.js
+++ src/Components/About.js
@@ -1,7 +1,18 @@
 import React from 'react'
-
+import { Tilt } from 'react-tilt'
 
 export default function About() {
+        const defaultOptions = {
+                reverse: false,  // reverse the tilt direction
+                max: 35,     // max tilt rotation (degrees)
+                perspective: 1000,   // Transform perspective, the lower the more extreme the tilt gets.
+                scale: 1.1,    // 2 = 200%, 1.5 = 150%, etc..
+                speed: 1000,   // Speed of the enter/exit transition
+                transition: true,   // Set a transition on enter/exit.
+                axis: null,   // What axis should be disabled. Can be X or Y.
+                reset: true,    // If the tilt effect has to be reset on exit.
+                easing: "cubic-bezier(.03,.98,.52,.99)",    // Easing on enter/exit.
+        }
         return (
                 <>
                         <div className="container-fluid pb-5 pb-md-0" style={{ minHeight: "100vh", backgroundColor: "#083749" }} id="about">
@@ -11,7 +22,9 @@ export default function About() {
                                         <h1 className="h2 mt-2 mb-4 mb-md-0 text-center" style={{ color: "#26B69F" }}><span style={{ borderBottom: "2px solid #26B69F", fontSize: "2.4rem" }}>About me</span></h1>
 
                                         <div className="col-md-5" data-aos="zoom-in-up">
-                                                <img src="me.png" alt="" className='img-fluid' />
+                                                <Tilt options={defaultOptions} >
+                                                        <img src="me.png" alt="" className='img-fluid' />
+                                                </Tilt>
                                         </div>
                                         <div className="col-md-7 mt-4 mt-md-5 pt-md-3 " style={{ color: "#26B69F", marginTop: "5rem", fontSize: "1.4rem" }} data-aos="fade-down">
                                                 <span>
